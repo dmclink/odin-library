53b92df30d11e075f72dff105b8d0c2aa23063f2
dmclink <135415819+dmclink@users.noreply.github.com>
2025-09-11 16:11:15 +0800

    feat: adding new books to array

--- src/js/library.js
+++ src/js/library.js
@@ -1,3 +1,43 @@
+const myLibrary = [];
+
+/** Book represents a book the user has submitted to their library.
+ *
+ * @param {string} title - the title of the book to add
+ * @param {string} author - the author's name of the new book
+ * @param {number} pages - the number of pages
+ * @param {boolean} read - whether or not the user has read the book
+ */
+function Book(title, author, pages, read) {
+	if (!new.target) {
+		throw new TypeError("Book constructor must be called with 'new'");
+	}
+
+	this.id = crypto.randomUUID();
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+/** Creates a new book from form inputs and appends it to myLibrary array.
+ */
+function addBookToLibrary() {
+	const form = document.querySelector('#add-new-form');
+	const formData = new FormData(form);
+	const newBook = new Book(
+		formData.get('new-title'),
+		formData.get('new-author'),
+		formData.get('new-pages'),
+		formData.get('new-read')
+	);
+
+	myLibrary.push(newBook);
+}
+
+function displayBooks() {
+	// TODO: grab some element from the DOM and fill it in with all the books in `myLibrary`
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	const dialog = document.querySelector('#dialog');
 	const openDialogBtn = document.querySelector('#open-dialog-btn');
@@ -15,34 +55,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	pagesRange.addEventListener('input', () => {
 		pagesOutput.value = pagesRange.value;
 	});
+
+	document.querySelector('#form-submit-btn').addEventListener('click', (e) => {
+		e.preventDefault();
+		addBookToLibrary();
+	});
 });
-
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
-	if (!new.target) {
-		throw new TypeError("Book constructor must be called with 'new'");
-	}
-
-	this.id = crypto.randomUUID();
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-}
-
-/** Creates a new book from inputs and appends it to myLibrary array.
- *
- * @param {string} title - the title of the book to add
- * @param {string} author - the author's name of the new book
- * @param {number} pages - the number of pages
- * @param {boolean} read - whether or not the user has read the book
- */
-function addBookToLibrary(title, author, pages, read) {
-	const newBook = new Book(title, author, pages, read);
-	myLibrary.push(newBook);
-}
-
-function displayBooks() {
-	// TODO: grab some element from the DOM and fill it in with all the books in `myLibrary`
-}
