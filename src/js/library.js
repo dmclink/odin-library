0373ad78835cb6454ce1b7fdcafb851543ca2f16
dmclink <135415819+dmclink@users.noreply.github.com>
2025-09-11 16:00:57 +0800

    feat: add new book dialog form

--- src/js/library.js
+++ src/js/library.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const dialog = document.querySelector('#dialog');
+	const openDialogBtn = document.querySelector('#open-dialog-btn');
+	openDialogBtn.addEventListener('click', () => {
+		dialog.showModal();
+	});
+
+	const closeDialogBtn = document.querySelector('#close-dialog-btn');
+	closeDialogBtn.addEventListener('click', () => {
+		dialog.close();
+	});
+
+	const pagesRange = document.querySelector('#new-pages');
+	const pagesOutput = document.querySelector('#new-pages-output');
+	pagesRange.addEventListener('input', () => {
+		pagesOutput.value = pagesRange.value;
+	});
+});
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,6 +31,13 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
+/** Creates a new book from inputs and appends it to myLibrary array.
+ *
+ * @param {string} title - the title of the book to add
+ * @param {string} author - the author's name of the new book
+ * @param {number} pages - the number of pages
+ * @param {boolean} read - whether or not the user has read the book
+ */
 function addBookToLibrary(title, author, pages, read) {
 	const newBook = new Book(title, author, pages, read);
 	myLibrary.push(newBook);
